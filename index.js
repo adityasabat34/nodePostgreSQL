0bd752d0ad6a7321e41842aebcc4a2fec1ace8b3
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-13 16:21:12 +0530

    databse set up done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const { Client } = require("pg");
+
+const connection = new Client({
+  host: "localhost",
+  user: "postgres",
+  password: "aditya@348",
+  port: 5432,
+  database: "prac",
+});
+
+connection
+  .connect()
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
