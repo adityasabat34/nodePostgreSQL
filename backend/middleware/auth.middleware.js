d58b2689c750cd60ed1c8d5b237d41ad9137dd4c
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-06-18 07:45:47 +0530

    create auth middleware security

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+import prisma from "../utils/prisma";
+import { ApiError } from "../utils/ApiError";
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.jwt;
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized - No token found");
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (!decoded) {
+    throw new ApiError(401, "Unauthorized - Invalid token");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: decoded.id,
+    },
+    select: {
+      id: true,
+      email: true,
+      isAdmin: true,
+      createdAt: true,
+    },
+  });
+
+  if (!user) {
+    throw new ApiError(401, "Unauthorized - User not found");
+  }
+
+  req.user = user;
+  next();
+});
+
+export { protect };
